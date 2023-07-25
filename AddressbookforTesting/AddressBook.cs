bc12b21c1aa9cd275ac7c7dbd86545ceecbb3fac
sukanya2712 <sukanyanaik511@gmail.com>
2023-07-25 12:32:28 +0530

    added files

---
+++ AddressbookforTesting/AddressBook.cs
@@ -0,0 +1,243 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookforTesting
+{
+    public class AddressBook 
+    { 
+    
+        List<Contact> ContactList = new List<Contact>();
+        MethodsforValidation validationMethods = new MethodsforValidation();
+      
+        string path = "C:\\Users\\Sukanay\\source\\repos\\AddressbookTestingPro\\AddressbookforTesting\\JSONFile.json";
+
+        public AddressBook()
+        {
+            ContactList = LoadContactsFromFile();
+        }
+
+        private List<Contact> LoadContactsFromFile()
+        {
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                var contacts = JsonConvert.DeserializeObject<List<Contact>>(json);
+                return contacts ?? new List<Contact>();
+            }
+
+            return new List<Contact>();
+        }
+
+        public bool AddContact(string name, string email, string phone, string state, string city, string zipcode)
+    {
+        if (!validationMethods.IsValidName(name))
+        {
+            Console.WriteLine(" INVALID NAME \n");
+            return false;
+        }
+
+        if (!validationMethods.IsValidEmail(email))
+        {
+            Console.WriteLine(" INVALID EMAIL \n");
+            return false;
+        }
+
+        if (!validationMethods.IsValidPhoneNumber(phone))
+        {
+            Console.WriteLine("INVALID PHONE NUMBER!\n");
+            return false;
+        }
+
+        if (!validationMethods.IsValidZIP(zipcode))
+        {
+            Console.WriteLine(" INVALID ZIP CODE! \n");
+            return false;
+        }
+
+        Contact contact = new Contact(name, email, phone, state, city, zipcode);
+
+        foreach (Contact c in ContactList)
+        {
+            if (c.Phone == phone || c.Name == name)
+            {
+                throw new ContactAlreadyExistsException("DUPLICATE CONTACT FOUND ");
+            }
+        }
+
+        ContactList.Add(contact);
+        Console.WriteLine("Contact Added...");
+        return true;
+    }
+
+    public Contact EditContact(string editName, string Data, int editType)
+    {
+        for (int index = 0; index < ContactList.Count; index++)
+        {
+            Contact contact = ContactList[index];
+            if (editName.Equals(contact.Name))
+            {
+                switch (editType)
+                {
+                    case 1:
+                        if (!validationMethods.IsValidName(Data))
+                        {
+                            Console.WriteLine("INVALID NAME!\n");
+                            return null;
+                        }
+                        contact.Name = Data;
+                        break;
+                    case 2:
+                        if (!validationMethods.IsValidEmail(Data))
+                        {
+                            Console.WriteLine("INVALID EMAIL!!! Enter the valid data...\n");
+                            return null;
+                        }
+                        contact.Email = Data;
+                        break;
+                    case 3:
+                        if (!validationMethods.IsValidPhoneNumber(Data))
+                        {
+                            Console.WriteLine("INVALID PHONE NUMBER!!! Enter the valid data...\n");
+                            return null;
+                        }
+                        contact.Phone = Data;
+                        break;
+                    case 4:
+                        contact.State = Data;
+                        break;
+                    case 5:
+                        contact.City = Data;
+                        break;
+                    case 6:
+                        if (!validationMethods.IsValidZIP(Data))
+                        {
+                            Console.WriteLine("INVALID ZIP CODE! .\n");
+                            return null; ; ;
+                        }
+                        contact.ZipCode = Data;
+                        break;
+                    default:
+                        Console.WriteLine("Enter a valid field!!!");
+                        break;
+                }
+                Console.WriteLine("Contact Edited!!!");
+                Console.WriteLine("Contact Details AFTER Edit:");
+                Console.WriteLine(contact.ToString());
+                return contact;
+            }
+        }
+        throw new Exception("CONTACT NOT FOUND!!! Please enter the name of an existing contact.");
+    }
+
+    public Contact DeleteContact(string deleteName)
+    {
+        for (int index = 0; index < ContactList.Count; index++)
+        {
+            Contact contact = ContactList[index];
+            if (deleteName == contact.Name)
+            {
+                ContactList.Remove(contact);
+                Console.WriteLine("Contact Deleted!");
+                return contact;
+            }
+        }
+        throw new Exception("CONTACT NOT FOUND!");
+    }
+
+
+        public Contact DisplayContact(string displayName)
+        {
+            for (int index = 0; index < ContactList.Count; index++)
+            {
+                Contact contact = ContactList[index];
+                if (displayName == contact.Name)
+                {
+                    Console.WriteLine("Details of the Contact: ");
+                    Console.WriteLine(contact);
+                    Console.WriteLine();
+                    return contact;
+                }
+            }
+            throw new Exception("CONTACT NOT FOUND!!! Please enter the name of an existing contact.");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("The Contacts in the Address Book are: ");
+            foreach (Contact contact in ContactList)
+            {
+                Console.WriteLine(contact.ToString());
+                Console.WriteLine();
+            }
+        }
+
+        public void AdddToJSONFile()
+        {
+            string jsonContent = JsonConvert.SerializeObject(ContactList);
+            File.WriteAllText(path, jsonContent);
+        }
+
+        public bool ReadFronJSONFile()
+        {
+            List<Contact> JSONFileList = new List<Contact>();
+
+            try
+            {
+                string fileContent = File.ReadAllText(path);
+                JSONFileList = JsonConvert.DeserializeObject<List<Contact>>(fileContent);
+                foreach (var contact in JSONFileList)
+                {
+                    Console.WriteLine(contact);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public void AddToCSVFile()
+        {
+            string path = "C:\\Users\\Sukanay\\source\\repos\\AddressbookTestingPro\\AddressbookforTesting\\CSVFile.csv";
+            StreamWriter Writer = new StreamWriter(path);
+            CsvWriter CSVwriter = new CsvWriter(Writer, CultureInfo.InvariantCulture);
+            CSVwriter.WriteRecords(ContactList);
+            CSVwriter.Dispose();
+            Writer.Close();
+        }
+
+        public bool ReadFromCSVFile()
+        {
+            List<Contact> CSVFileList = new List<Contact>();
+            string path = "C:\\Users\\Sukanay\\source\\repos\\AddressbookTestingPro\\AddressbookforTesting\\CSVFile.csv";
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                CsvReader csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                CSVFileList = csvReader.GetRecords<Contact>().ToList();
+                foreach (Contact contact in CSVFileList)
+                {
+                    Console.WriteLine(contact);
+                    Console.WriteLine();
+                }
+                reader.Close();
+                csvReader.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ AddressbookforTesting/Program.cs
@@ -0,0 +1,208 @@
+﻿namespace AddressbookforTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            Console.WriteLine("Welcome to AddressBook : ");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("******************MENU:******************");
+                Console.WriteLine("=> To Add Contact: PRESS 1");
+                Console.WriteLine("=> To Edit an Existing Contact: PRESS 2");
+                Console.WriteLine("=> To Delete a Contact: PRESS 3");
+                Console.WriteLine("=> To Display a specific Contact: PRESS 4");
+                Console.WriteLine("=> To Display all Contacts in the Address Book: PRESS 5");
+                Console.WriteLine("=> To Read the Contacts from the CSV File: PRESS 6");
+                Console.WriteLine("=> To Read the Contacts from the JSON File: PRESS 7");
+                Console.WriteLine("=> To EXIT: PRESS 8");
+               
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            try
+                            {
+                                bool isAdded = false;
+                                while (!isAdded)
+                                {
+                                    Console.WriteLine("Enter the details to add a contact: ");
+
+                                    Console.Write("\nEnter name: ");
+                                    string name = Console.ReadLine();
+
+                                    Console.Write("Enter email: ");
+                                    string email = Console.ReadLine();
+
+                                    Console.Write("Enter phone number: ");
+                                    string phone = Console.ReadLine();
+
+                                    Console.Write("Enter state: ");
+                                    string state = Console.ReadLine();
+
+                                    Console.Write("Enter city: ");
+                                    string city = Console.ReadLine();
+
+                                    Console.Write("Enter zip: ");
+                                    string zip = Console.ReadLine();
+
+                                    isAdded = addressBook.AddContact(name, email, phone, state, city, zip);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter the First Name of the Contact you want to Edit: ");
+                                String editName = Console.ReadLine();
+
+                                addressBook.DisplayContact(editName);
+
+                                Console.WriteLine("Select the name of the Field you want to Edit in Contact's Details:");
+                                Console.WriteLine("1.First Name\t2.E-mail Address\t3.Phone Number\t4.State\t5.City\t6.ZIP Code");
+                                int option = Convert.ToInt32(Console.ReadLine());
+
+                                string newData = null;
+                                switch (option)
+                                {
+                                    case 1:
+                                        Console.Write("Enter the NEW Name: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    case 2:
+                                        Console.Write("Enter the NEW E-mail Address: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    case 3:
+                                        Console.Write("Enter the NEW Phone Number: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    case 4:
+                                        Console.Write("Enter the NEW State: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    case 5:
+                                        Console.Write("Enter the NEW City: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    case 6:
+                                        Console.Write("Enter the NEW ZIP Code: ");
+                                        newData = Console.ReadLine();
+                                        break;
+                                    default:
+                                        Console.WriteLine("Enter a valid field!!!");
+                                        break;
+                                }
+                                if (newData != null)
+                                {
+                                    Contact contact = addressBook.EditContact(editName, newData, option);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter the Name of the Contact you want to Delete: ");
+                                string deleteName = Console.ReadLine();
+
+                                addressBook.DisplayContact(deleteName);
+                                Console.WriteLine("Are you sure you want to DELETE the Contact?");
+                                Console.WriteLine("1. YES \t 2. NO");
+                                int option = Convert.ToInt32(Console.ReadLine());
+                                switch (option)
+                                {
+                                    case 1:
+                                        Contact contact = addressBook.DeleteContact(deleteName);
+                                        break;
+                                    case 2:
+                                        Console.WriteLine("Contact is NOT deleted!!!");
+                                        break;
+                                    default:
+                                        Console.WriteLine("Select a valid option!!!");
+                                        break;
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            try
+                            {
+                                Console.WriteLine("Enter the Name of the Contact you want to Display: ");
+                                string displayName = Console.ReadLine();
+
+                                Contact contact = addressBook.DisplayContact(displayName);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            try
+                            {
+                                addressBook.Display();
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            bool read = addressBook.ReadFromCSVFile();
+                            if (!read)
+                            {
+                                Console.WriteLine("ERROR!!!Could Not Read The Contacts From CSV File");
+                            }
+                            break;
+                        }
+                    case 7:
+                        {
+                            bool read = addressBook.ReadFronJSONFile();
+                            if (!read)
+                            {
+                                Console.WriteLine("ERROR!!!Could Not Read The Contacts From JSON File.");
+                            }
+                            break;
+                        }
+                    case 8:
+                        {
+                            addressBook.AdddToJSONFile();
+                            addressBook.AddToCSVFile();
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid Choice!!! Please make a valid choice.");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+    }
